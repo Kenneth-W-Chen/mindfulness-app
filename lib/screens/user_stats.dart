13702462f7e5ea22835655782dcd93b4c662ce98
kenneth-w-chen <74205780+Kenneth-W-Chen@users.noreply.github.com>
2025-03-15 10:14:31 -0500

    Add favorite activity to user stats
    Adjust layout

--- lib/screens/user_stats.dart
+++ lib/screens/user_stats.dart
@@ -12,7 +12,7 @@ class UserStats extends StatefulWidget {
   State<StatefulWidget> createState() => _UserStatsState();
 }
 
-class _UserStatsState extends State<UserStats> {
+class _UserStatsState extends State<UserStats>  with TickerProviderStateMixin {
   int theme = 0;
 
   final Color _cardColorLight = Colors.white;
@@ -22,20 +22,50 @@ class _UserStatsState extends State<UserStats> {
 
   String achiementsCompleted = "0";
   String dailiesCompleted = "0";
-  String favoriteActivity = "Meditation Station";
+  bool hasActivityLogs = true;
+  String favoriteActivity = "You haven't completed any activities yet.";
+  String favoriteActivitySubtext = "Keep playing to find out what your favorite activity is.";
+  String favoriteActivityCount = "?";
+
+  late AnimationController _animationController;
+  late Animation<double> _growAnimation;
 
   @override
   void initState() {
     super.initState();
+    _animationController = AnimationController(duration: const Duration(seconds: 5), vsync: this)..repeat(reverse: true);
+    _growAnimation = Tween<double>(begin: .2, end: .45).animate(_animationController);
     asyncInitState();
+  }
+
+  @override
+  void dispose(){
+    _animationController.dispose();
+    super.dispose();
   }
 
   Future<void> asyncInitState() async {
     theme = (await widget.storage.getPreferences([PreferenceName.theme]))![PreferenceName.theme] as int;
+    // doing a setState here since fetching the other data can take a while
+    setState(() {
+
+    });
+
     achiementsCompleted = '${await widget.storage.achievementCount()}';
     dailiesCompleted = '${await widget.storage.getDailyActivityCompletionCount()}';
     var activityLogCounts = await widget.storage.getActivityLogCount();
-    favoriteActivity = ActivityName.values[activityLogCounts[0]['activity_id'] as int].toString();
+    var activityLogCount = activityLogCounts[0]['cnt'] as int;
+    hasActivityLogs = activityLogCount > 0;
+    if(hasActivityLogs) {
+      favoriteActivity = "Your favorite activity is ${ActivityName.values[activityLogCounts[0]['activity_id'] as int].toString()}.";
+      favoriteActivitySubtext = "You've completed it $activityLogCount time";
+      if(activityLogCount>1){
+        favoriteActivitySubtext += 's';
+      }
+      favoriteActivitySubtext += '.';
+      favoriteActivityCount = activityLogCount.toString();
+
+    }
     setState(() {
 
     });
@@ -55,7 +85,7 @@ class _UserStatsState extends State<UserStats> {
         children: [
           // Stat: Total days since first app open
           Card(
-            color: theme == 1 ? _cardColorDark : _cardColorLight,
+            color: _getCardColor(),
             clipBehavior: Clip.antiAliasWithSaveLayer,
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
@@ -79,74 +109,88 @@ class _UserStatsState extends State<UserStats> {
           ),
           // Second row of stats
           Row(
-            children:[
-              // Stat: Number of achievements completed
-              Expanded(
-                child: Card(
-                  color: theme == 1 ? _cardColorDark : _cardColorLight,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Stack(
-                        alignment: Alignment.center,
+              children:[
+                // Stat: Number of achievements completed
+                Expanded(
+                    child: Card(
+                      color: _getCardColor(),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
                         children: [
-                          const Image(image: AssetImage('assets/icons/trophy.png'), width: 150,),
+                          const Text('\r'), // added to force card heights to match... intrinsic height increases the height by 20 pixels
+                          Stack(
+                            alignment: Alignment.center,
+                            children: [
+                              const Image(image: AssetImage('assets/icons/trophy.png'), width: 150,),
+                              Padding(
+                                  padding: const EdgeInsets.only(bottom:40.0),
+                                  child: Text(
+                                    achiementsCompleted,
+                                    textAlign: TextAlign.center,
+                                    style: const TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 20,
+                                    ),
+                                  )
+                              )
+                            ],
+                          ),
                           Padding(
-                              padding: const EdgeInsets.only(bottom: 40.0),
-                              child: Text(
-                                achiementsCompleted,
+                            padding: const EdgeInsets.all(10.0),
+                            child: Text('Achievements Completed', style: _getTextStyle()),
+                          )
+                        ],
+                      ),
+                    )
+                ),
+                // Stat: Favorite Activity
+                Expanded(
+                    child: Card(
+                      color: _getCardColor(),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Text(favoriteActivity, style: _getTextStyle(), textAlign: TextAlign.center,),
+                          Stack(
+                            alignment: Alignment.center,
+                            children: [
+                              const Image(image: AssetImage('assets/icons/meditate.png'), width: 150,),
+                              AnimatedBuilder(
+                                  animation: _growAnimation,
+                                  builder: (context,child){
+                                    return Container(
+                                      width: 50,
+                                      height: 50,
+                                      decoration: BoxDecoration(
+                                        shape: BoxShape.circle,
+                                        gradient: RadialGradient(
+                                          radius: 1,
+                                          stops: [.1,_growAnimation.value,.9],
+                                          colors: const [Color(0xCC3B59DB), Color(0xAA508BC7), Color(0x3300D8FF)],
+                                        ),
+                                      ),
+                                    );
+                                  }
+                              ),
+                              Text(
+                                favoriteActivityCount,
                                 textAlign: TextAlign.center,
                                 style: const TextStyle(
                                   fontWeight: FontWeight.bold,
                                   fontSize: 20,
+                                  color: Colors.white
                                 ),
                               )
-                          )
+                            ],
+                          ),
+                          Text(favoriteActivitySubtext, style: _getTextStyle(), textAlign: TextAlign.center,),
                         ],
                       ),
-                      Padding(
-                        padding: const EdgeInsets.all(10.0),
-                        child: Text('Achievements Completed', style: _getTextStyle()),
-                      )
-                    ],
-                  ),
-                )
-              ),
-              // Stat: Favorite Activity todo
-              Expanded(
-                  child: Card(
-                    color: theme == 1 ? _cardColorDark : _cardColorLight,
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        Stack(
-                          alignment: Alignment.center,
-                          children: [
-                            const Image(image: AssetImage('assets/icons/trophy.png'), width: 150,),
-                            Padding(
-                                padding: const EdgeInsets.only(bottom: 40.0),
-                                child: Text(
-                                  achiementsCompleted,
-                                  textAlign: TextAlign.center,
-                                  style: const TextStyle(
-                                    fontWeight: FontWeight.bold,
-                                    fontSize: 20,
-                                  ),
-                                )
-                            )
-                          ],
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.all(10.0),
-                          child: Text('Achievements Completed', style: _getTextStyle()),
-                        )
-                      ],
-                    ),
-                  )
-              ),
-            ]
+                    )
+                ),
+              ]
           ),
           // Third row of stats
           Row(
@@ -154,7 +198,7 @@ class _UserStatsState extends State<UserStats> {
               // Stat: Times all daily activities have been completed
               Expanded(
                 child: Card(
-                  color: theme == 1 ? _cardColorDark : _cardColorLight,
+                  color: _getCardColor(),
                   child: Column(
                     mainAxisAlignment: MainAxisAlignment.center,
                     crossAxisAlignment: CrossAxisAlignment.center,
@@ -164,57 +208,57 @@ class _UserStatsState extends State<UserStats> {
                         child: Text("You've completed your", textAlign: TextAlign.center, style: _getTextStyle()),
                       ),
                       Stack(
-                          alignment: Alignment.center,
-                          children: [
-                            const Image(
-                              image: AssetImage('assets/icons/calendar.png'),
+                        alignment: Alignment.center,
+                        children: [
+                          const Image(
+                            image: AssetImage('assets/icons/calendar.png'),
+                            width: 150,
+                          ),
+                          Container(
+                              alignment: Alignment.center,
                               width: 150,
-                            ),
-                            Container(
-                                alignment: Alignment.center,
-                                width: 150,
-                                height: 150,
-                                child: Column(
-                                  mainAxisAlignment: MainAxisAlignment.center,
+                              height: 150,
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
 
-                                  children: [
-                                    const Expanded(
-                                        flex: 2,
-                                        child: Padding(
-                                            padding: EdgeInsets.only(
-                                                top: 7),
-                                            child: Text(
-                                              "Dailies",
-                                              textAlign: TextAlign.center,
-                                              style: TextStyle(
+                                children: [
+                                  const Expanded(
+                                      flex: 2,
+                                      child: Padding(
+                                          padding: EdgeInsets.only(
+                                              top: 7),
+                                          child: Text(
+                                            "Dailies",
+                                            textAlign: TextAlign.center,
+                                            style: TextStyle(
                                                 fontWeight: FontWeight.bold,
                                                 fontSize: 20,
                                                 color: Colors.white
-                                              ),
-                                            )
+                                            ),
+                                          )
+                                      )
+                                  ),
+                                  Expanded(
+                                    flex: 8,
+                                    child: Align(
+                                        child: Text(
+                                          dailiesCompleted,
+                                          textAlign: TextAlign.center,
+                                          style: const TextStyle(
+                                            fontWeight: FontWeight.bold,
+                                            fontSize: 20,
+                                          ),
                                         )
                                     ),
-                                    Expanded(
-                                        flex: 8,
-                                        child: Align(
-                                            child: Text(
-                                              dailiesCompleted,
-                                              textAlign: TextAlign.center,
-                                              style: const TextStyle(
-                                                fontWeight: FontWeight.bold,
-                                                fontSize: 20,
-                                              ),
-                                            )
-                                        ),
-                                    )
-                                  ],
-                                )
-                            )
-                          ],
-                        ),
-                        Padding(
+                                  )
+                                ],
+                              )
+                          )
+                        ],
+                      ),
+                      Padding(
                         padding: const EdgeInsets.all(10.0),
-                        child: Text(int.parse(dailiesCompleted) == 1 ? 'time':'times', style: _getTextStyle()),
+                        child: Text(int.parse(dailiesCompleted) == 1 ? 'time.':'times.', style: _getTextStyle()),
                       )
                     ],
                   ),
@@ -223,7 +267,7 @@ class _UserStatsState extends State<UserStats> {
               // Stat: Longest daily activity completion streak todo
               Expanded(
                 child: Card(
-                  color: theme == 1 ? _cardColorDark : _cardColorLight,
+                  color: _getCardColor(),
                   child: Column(
                     mainAxisAlignment: MainAxisAlignment.center,
                     crossAxisAlignment: CrossAxisAlignment.center,
@@ -263,6 +307,8 @@ class _UserStatsState extends State<UserStats> {
       )
     );
   }
+
+  Color _getCardColor() => theme == 1 ? _cardColorDark : _cardColorLight;
 
   TextStyle _getTextStyle() => theme == 1 ? _fontColorDark : _fontColorLight;
 }
