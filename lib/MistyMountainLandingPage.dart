7f1003093d8bca560cfa389f1b5628ea1f2f7b2b
Eichelle Turner <turnereichelle@gmail.com>
2025-04-04 16:50:43 -0500

    Updated UI
    I updated all the UI for both Misty Mountain and Tranquil Forest and fixed the descriptions for all 6 activities and add a background overlay at 50% opacity

--- lib/MistyMountainLandingPage.dart
+++ lib/MistyMountainLandingPage.dart
@@ -9,25 +9,49 @@ class MistyMountainLandingPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final List<Map<String, dynamic>> activities = [
+      {
+        'title': 'Meditation Station',
+        'icon': Icons.headset,
+        'description':
+            "Take a few moments to escape from the daily noise and find your inner calm, with guided meditations.",
+        'builder': (context) => const MeditationStation(),
+      },
+      {
+        'title': 'Twilight Alley',
+        'icon': Icons.flag,
+        'description':
+            "Track your emotions, write down your reflections, and explore your inner world in a peaceful setting.",
+        'builder': (context) => const TwilightAlleyIntro(),
+      },
+      {
+        'title': 'Calming Cliff',
+        'icon': Icons.landscape,
+        'description':
+            "View the stunning sight that offers a moment to reconnect with yourself and the vastness of nature.",
+        'builder': (context) => const CalmingCliffsIntro(),
+      }
+    ];
+
     return Scaffold(
       appBar: AppBar(
         title: const Text(
           'Misty Mountain',
           style: TextStyle(
             fontWeight: FontWeight.bold,
-            fontSize: 24,
+            fontSize: 30,
             letterSpacing: 1.2,
           ),
         ),
-        backgroundColor: const Color.fromARGB(255, 133, 23, 148),
+        backgroundColor: const Color.fromARGB(255, 56, 130, 107),
         elevation: 0,
       ),
       body: Container(
         decoration: BoxDecoration(
           gradient: LinearGradient(
             colors: [
-              const Color.fromARGB(255, 149, 80, 159)!,
-              Colors.teal[400]!
+              const Color.fromARGB(104, 7, 153, 87),
+              const Color.fromARGB(206, 0, 128, 128),
             ],
             begin: Alignment.topLeft,
             end: Alignment.bottomRight,
@@ -36,63 +60,79 @@ class MistyMountainLandingPage extends StatelessWidget {
         child: Padding(
           padding: const EdgeInsets.all(20.0),
           child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center, // Center alignment
             children: [
               const Text(
                 'Welcome to Misty Mountain!',
                 style: TextStyle(
-                  fontSize: 28,
+                  fontSize: 32, // Larger font size for prominence
                   color: Colors.white,
                   fontWeight: FontWeight.bold,
-                  letterSpacing: 1.2,
+                  letterSpacing: 1.5,
                 ),
+                textAlign: TextAlign.center, // Center text
               ),
-              const SizedBox(height: 20),
+              const SizedBox(height: 40),
               const Text(
                 'Explore our calming activities:',
                 style: TextStyle(
-                  fontSize: 22,
+                  fontSize: 25,
                   color: Colors.white70,
                   fontStyle: FontStyle.italic,
                 ),
+                textAlign: TextAlign.center, // Center text
               ),
-              const SizedBox(height: 30),
-
-              // Activity for Meditation Station
-              activityCard('Meditation Station', context, Icons.headset,
-                  'Listen to calming sounds and nature noises.',
-                  builder: (context) => const MeditationStation(),
-                  // colors
-                  cardColor: Colors.white.withOpacity(0.9),
-                  shadowColor: Colors.black.withOpacity(0.3),
-                  iconBackgroundColor: Colors.teal[700],
-                  iconColor: Colors.white,
-                  textColor: Colors.teal[700],
-                  subTextColor: Colors.teal[600]),
-
-              // Activity for Goal Setting
-              activityCard('Twilight Alley', context, Icons.flag,
-                  'Journal some of your thoughts and track your mood.',
-                  builder: (context) => const TwilightAlleyIntro(),
-                  // colors
-                  cardColor: Colors.white.withOpacity(0.9),
-                  shadowColor: Colors.black.withOpacity(0.3),
-                  iconBackgroundColor: Colors.teal[700],
-                  iconColor: Colors.white,
-                  textColor: Colors.teal[700],
-                  subTextColor: Colors.teal[600]),
-
-              // Activity for calming visualization
-              activityCard('Calming Cliff', context, Icons.landscape,
-                  'Calm yourself and realize that there is so much out there.',
-                  builder: (context) => const CalmingCliffsIntro(),
-                  // colors
-                  cardColor: Colors.white.withOpacity(0.9),
-                  shadowColor: Colors.black.withOpacity(0.3),
-                  iconBackgroundColor: Colors.teal[700],
-                  iconColor: Colors.white,
-                  textColor: Colors.teal[700],
-                  subTextColor: Colors.teal[600]),
+              const SizedBox(height: 40),
+              Expanded(
+                child: ListView(
+                  children: activities.map((activity) {
+                    return Padding(
+                      padding: const EdgeInsets.only(bottom: 20),
+                      child: SizedBox(
+                        width: double.infinity,
+                        height: 200,
+                        child: Container(
+                          decoration: BoxDecoration(
+                            image: DecorationImage(
+                              image: AssetImage(
+                                  'assets/images/mountain.png'), // Background image
+                              fit: BoxFit.cover,
+                              colorFilter: ColorFilter.mode(
+                                Colors.black.withOpacity(
+                                    0.4), // Reduced opacity for better visibility
+                                BlendMode.dstATop,
+                              ),
+                            ),
+                            borderRadius: BorderRadius.circular(15),
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.black.withOpacity(0.3),
+                                blurRadius: 5,
+                                spreadRadius: 2,
+                                offset: const Offset(3, 3),
+                              ),
+                            ],
+                          ),
+                          child: activityCard(
+                            activity['title'],
+                            context,
+                            activity['icon'],
+                            activity['description'],
+                            builder: activity['builder'],
+                            cardColor: Colors.transparent,
+                            shadowColor: Colors.transparent,
+                            iconBackgroundColor:
+                                const Color.fromARGB(255, 7, 47, 16),
+                            iconColor: Colors.white,
+                            textColor: Colors.white,
+                            subTextColor: Colors.white70,
+                          ),
+                        ),
+                      ),
+                    );
+                  }).toList(),
+                ),
+              ),
             ],
           ),
         ),
